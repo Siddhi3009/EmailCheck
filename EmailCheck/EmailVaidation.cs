80d0609f278c214f860a94f28412ee28d51dc7ae
Administrator <siddhiseth3009@gmail.com>
2020-10-07 01:40:52 +0530

    [Siddhi]Refactor Regex statement edited

--- EmailCheck/EmailVaidation.cs
+++ EmailCheck/EmailVaidation.cs
@@ -7,7 +7,7 @@ namespace EmailCheck
 {
     class EmailVaidation
     {
-        public static string REGEX_EMAIL = "^(abc)[_+-.]?[a-z0-9]{0,10}?@[a-z].[a-z][.]?[a-z]{2}?";
+        public static string REGEX_EMAIL = "^(abc)[_+-.]?[a-z0-9]{0,}?@[a-z].[a-z][.]?[a-z]{2}?";
         public bool validateEmail(string email)
         {
             return Regex.IsMatch(email, REGEX_EMAIL);
