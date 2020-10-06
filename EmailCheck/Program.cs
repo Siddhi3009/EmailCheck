cad8239d6247fcec76f072471922f3bbb5d4b37d
Administrator <siddhiseth3009@gmail.com>
2020-10-06 19:17:30 +0530

    [Siddhi]Add EmailCheck program added with welcome message

---
+++ EmailCheck/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmailCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Email Check program");
+        }
+    }
+}
