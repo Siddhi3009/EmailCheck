7ad0dd9408dcbe741672addbae71d9c1b99e0016
Administrator <siddhiseth3009@gmail.com>
2020-10-07 23:04:59 +0530

    [Siddhi]Refactor testing loop removed

--- EmailCheck/Program.cs
+++ EmailCheck/Program.cs
@@ -5,19 +5,13 @@ namespace EmailCheck
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             Console.WriteLine("Welcome to Email Check program");
             EmailVaidation validation = new EmailVaidation();
-            int loop = 1;
-            while (loop == 1)
-            {
-                Console.WriteLine("Enter you email");
-                string email = Console.ReadLine();
-                bool valid = validation.validateEmail(email);
-                Console.WriteLine(valid);
-                Console.WriteLine("1. More\n2. End");
-                loop = int.Parse(Console.ReadLine());
-            }
+            Console.WriteLine("Enter you email");
+            string email = Console.ReadLine();
+            bool valid = validation.validateEmail(email);
+            Console.WriteLine(valid);
         }
     }
 }
